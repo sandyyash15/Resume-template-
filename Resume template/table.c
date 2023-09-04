97678a7a0db04be0586ee92f11f20807a4f5a4aa
Yash Saraswat <142895197+sandyyash15@users.noreply.github.com>
2023-09-04 11:26:56 +0530

    Add files via upload

---
+++ Resume template/table.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+   // int i = 1;
+    // for(i = 1; i<= 10; i++)
+    // {
+    //     printf("%d\n", i);
+    // }
+
+    // int i  = 1;
+    // while(i <= 10)
+    // {
+    //     printf("%d\n", i);
+    //     i++;
+    // }
+
+    int i = 1;
+    do {
+        printf("%d\n", i);
+    }
+    while(i <= 10)
+    return 0;
+}
